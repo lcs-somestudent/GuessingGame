e75f20893889b113b793daf611c8c8d1418a0255
Russell Gordon <rgordon@lcs.on.ca>
2020-11-17 14:45:12 -0500

    Initial Commit

---
+++ GuessingGame/GuessingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessingGameApp.swift
+//  GuessingGame
+//
+//  Created by Russell Gordon on 2020-11-17.
+//
+
+import SwiftUI
+
+@main
+struct GuessingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
